270cc7273682221790f09cdfdaba90de8ee6cf73
v0vchic <vladimir.bogdanovi04@gmail.com>
2026-02-10 12:02:01 +0300

    feat: add Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import AboutUs from '@/components/pages/about-us'
 import Hero from '@/components/pages/hero'
+import Services from '@/components/pages/services'
+import Contact from '@/components/pages/сontact'
 
-export default Hero
+export default function Page() {
+  return (
+    <main>
+      <section id="hero">
+        <Hero />
+      </section>
+
+      <section id="about">
+        <AboutUs />
+      </section>
+
+      <section id="services">
+        <Services />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
+    </main>
+  )
+}
--- components/layout/Header/ui.tsx
+++ components/layout/Header/ui.tsx
@@ -1,38 +1,47 @@
 'use client'
 
 import Image from 'next/image'
-import Link from "next/link";
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export const Header = () => {
-    return (
-        <header className="w-full fixed top-0 left-0 bg-white">
-            <div className="flex items-center justify-between p-4">
-                <Link href="/" scroll={true} className="block">
-                    <Image
-                        src="/images/logo.svg"
-                        alt="Marvel Power Group"
-                        width={146}
-                        height={55}
-                        priority
-                    />
-                </Link>
+  const [isScrolled, setIsScrolled] = useState(false)
 
-                <nav className="space-x-8 text-lg font-semibold">
-                    <Link href="#about">
-                        About
-                    </Link>
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0)
+    }
 
-                    <Link href="#services">
-                        Services
-                    </Link>
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+  return (
+    <header className={`fixed top-0 left-0 w-full z-50 flex items-center justify-between py-7 px-[264px] transition-colors duration-300 ${
+      isScrolled ? 'bg-white shadow-md' : 'bg-transparent'
+    }`}
+    >
+      <Link href="/">
+        <Image
+          src="/images/logo.svg"
+          alt="Marvel Power Group"
+          width={146}
+          height={55}
+        />
+      </Link>
 
-                    <Link href="#contact">
-                        Contact
-                    </Link>
-                </nav>
+      <nav className="flex gap-16 font-normal">
+        <Link href="#about">
+          About
+        </Link>
 
-            </div>
-        </header>
-    );
+        <Link href="#services">
+          Services
+        </Link>
 
+        <Link href="#contact">
+          Contact
+        </Link>
+      </nav>
+    </header>
+  )
 }
